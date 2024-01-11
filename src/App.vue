d554cf31850ebcada49ac3205bd9093d3fa0077d
Иван Шолохов <Neron41k@yandex.ru>
2024-01-11 16:17:07 +0900

    ver 1.3: 1. Верстка поиска👌

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <script setup>
+import { onMounted } from 'vue'
+import axios from 'axios'
+
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
 import Drawer from './components/Drawer.vue'
+
+onMounted(() => {
+  axios.get('').then((resp) => console.log(resp.data))
+})
 </script>
 
 <template>
@@ -10,9 +17,28 @@ import Drawer from './components/Drawer.vue'
     <Header />
 
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все книги</h2>
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">Все книги</h2>
 
-      <CardList />
+        <div class="flex gap-4">
+          <!--<select class="py-2 px-3 border rounded-md outline-none">
+            <option>По названию</option>
+          </select> хуй знает зачем здесь сортировка💩 -->
+
+          <div class="relative">
+            <img class="absolute left-3 top-3" src="/search.svg" alt="Search" />
+            <input
+              class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+              type="text"
+              placeholder="Поиск..."
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="mt-10">
+        <CardList />
+      </div>
     </div>
   </div>
 </template>
