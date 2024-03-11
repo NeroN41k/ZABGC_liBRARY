5dc6d983121bad2ef9eeb81fcf794f29f93bbe95
Ivan <neron41k@yandex.ru>
2024-03-11 09:36:47 +0900

    start profile front

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,3 +1,13 @@
 <template>
-  <h2 class="text-3xl font-bold mb-8">Профиль</h2>
+  <div class="flex justify-between items-center">
+    <h2 class="text-3xl font-bold mb-8">Профиль</h2>
+  </div>
+
+  <div class="text-center gap-14">
+    <img src="/ProfileChange.svg" alt="Profile" class="w-36" />
+    <div class="flex gap-1 text-xl py-3">
+      <b>Шолохов</b>
+      <b>Иван</b>
+    </div>
+  </div>
 </template>
